2c2c4dbf3f0b03a13ee26f8194ccee515bf4e221
juanudk <juanudkp@gmail.com>
2019-12-26 19:40:02 -0300

    Commit Inicial

--- app03/src/App.js
+++ app03/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import { store } from './store'
+import store from './store';
 
-const App = () => {
+const App = () => (
   <Provider store={store}>
     <h1>Hello World</h1>
   </Provider>
-}
+);
 
 export default App;
